5bc547163cbc4280226de064c30e405d59fc6463
Velezmidi <velez.midi@gmail.com>
2021-09-03 09:06:59 -0500

    kdjsk

---
+++ loopingTriangle.js
@@ -0,0 +1,8 @@
+const looping = (number) => {
+  for (let i = 0; i <= number; i++) {
+    console.log("#".repeat(i));
+  }
+};
+
+looping(7);
+// looping(3);
--- typeco.js
+++ typeco.js
@@ -1,2 +1,3 @@
 console.log(+"5" + 1);
 //ewe
+console.log("nimergas");
